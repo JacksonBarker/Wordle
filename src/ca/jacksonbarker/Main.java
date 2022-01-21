a9e5ab5afc64e924d7256af9c1123c62b9be1bb9
Jackson Barker <jackson@jacksonbarker.ca>
2022-01-21 13:28:38 -0500

    WIP

--- src/ca/jacksonbarker/Main.java
+++ src/ca/jacksonbarker/Main.java
@@ -1,8 +1,46 @@
 package ca.jacksonbarker;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scnr = new Scanner(System.in);
+        String[] guesses = new String[6];
+
+        System.out.println("Please enter a 5 letter word:");
+        String input = scnr.next();
+
+        while (guesses[5] == null) {
+            System.out.println("Please enter a 5 letter word:");
+            input = scnr.next();
+
+            if (input.length() != 5) {
+                System.out.println("Please enter a 5 letter word:");
+                input = scnr.next();
+            }
+
+            for (int i = 0; i < 6; i++) {
+                if (guesses[i] == null) {
+                    guesses[i] = input;
+                    input = null;
+                }
+            }
+
+            printBoard(guesses);
+
+        }
+
+    }
+
+    public static void printBoard(String[] guesses) {
+        System.out.println();
+        for (int i = 0; i < 6; i++) {
+            if (guesses[i] != null) {
+                System.out.println(guesses[i]);
+            } else {
+                System.out.println("☐☐☐☐☐");
+            }
+        }
     }
 }
