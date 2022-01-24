7e3aeb52cc7023ba8051e67d664959ce7ece3a10
Jackson Barker <jackson@jacksonbarker.ca>
2022-01-24 14:28:44 -0500

    first "finished" version (cant win or lose)

--- src/ca/jacksonbarker/Main.java
+++ src/ca/jacksonbarker/Main.java
@@ -3,25 +3,32 @@ package ca.jacksonbarker;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
 import javax.swing.*;
 
 public class Main extends JFrame implements KeyListener {
 
+    public Main() {addKeyListener(this);}
+
+    public static JLabel[][] grid = new JLabel[6][5];
+
+    public static int activeLine = 0;
+
+    public static final int[] wordLetters = new int[26];
+    public static int[] inputLetters = new int[26];
+
+    //public static final String word = WordList.wordList()[new Random().nextInt(WordList.wordList().length)];
+    public static String word = "hello";
+
     public static void main(String[] args) {
-        int gameWidth = 5;
-        int gameHeight = 6;
-
-        JLabel[][] grid = new JLabel[gameHeight][gameWidth];
-
-        JFrame game = new JFrame();
-        game.setSize(gameWidth * 100, gameHeight * 100);
+        Main game = new Main();
+        game.setSize(500, 600);
         game.setTitle("WORDLE");
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        game.setLayout(new GridLayout(0, gameWidth));
-        for (int i = 0; i < gameHeight; i++) {
-            for (int j = 0; j < gameWidth; j++) {
+        game.setLayout(new GridLayout(0, 5));
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
                 grid[i][j] = new JLabel("", SwingConstants.CENTER);
                 grid[i][j].setFont(new Font("Comic Sans MS", Font.BOLD, 64));
                 grid[i][j].setOpaque(true);
@@ -30,64 +37,17 @@ public class Main extends JFrame implements KeyListener {
         }
         game.setVisible(true);
 
-        Scanner scnr = new Scanner(System.in);
-        String[] guesses = new String[gameHeight];
-        //String word = WordList.wordList()[new Random().nextInt(WordList.wordList().length)];
-        String word = "hello";
-        String input;
-        int[] wordLetters = new int[26];
-        int[] inputLetters = new int[26];
-
-        for (int i = 0; i < gameWidth; i++) {
+        for (int i = 0; i < 5; i++) {
             for (int j = 0; j < wordLetters.length; j++) {
                 if (word.charAt(i) == j + 97) {
                     wordLetters[j] += 1;
                 }
             }
         }
-
-        while (guesses[gameHeight - 1] == null) {
-            input = "";
-
-            while (!isValidWord(input, gameWidth)) {
-                System.out.println("Enter a 5 letter word:");
-                input = scnr.next().toLowerCase();
-            }
-
-            for (int i = 0; i < gameHeight; i++) {
-                if (guesses[i] == null) {
-                    guesses[i] = input;
-                    input = null;
-                }
-            }
-
-            for (int i = 0; i < gameHeight; i++) {
-                if (guesses[i] != null) {
-                    for (int j = 0; j < gameWidth; j++) {
-                        grid[i][j].setText(guesses[i].charAt(j) + "");
-                        if (word.charAt(j) == guesses[i].charAt(j)) {
-                            grid[i][j].setBackground(Color.green);
-                        } else {
-                            grid[i][j].setBackground(Color.gray);
-                        }
-                    }
-                }
-            }
-
-            for (int i = 0; i < gameHeight; i++) {
-                if (guesses[i] != null && guesses[i].equals(word)) {
-                    JOptionPane.showMessageDialog(game, "You guessed the Wordle!");
-                    System.exit(0);
-                }
-            }
-        }
-
-        JOptionPane.showMessageDialog(game, "The Wordle was: " + word);
-        System.exit(0);
     }
 
-    public static boolean isValidWord(String word, int gameWidth) {
-        if (word.length() != gameWidth) {
+    public static boolean isValidWord(String word) {
+        if (word.length() != 5) {
             return false;
         }
 
@@ -110,60 +70,102 @@ public class Main extends JFrame implements KeyListener {
     public void keyReleased(KeyEvent keyEvent) {
         switch (keyEvent.getKeyCode()) {
             case KeyEvent.VK_A:
+                input('a');
                 break;
             case KeyEvent.VK_B:
+                input('b');
                 break;
             case KeyEvent.VK_C:
+                input('c');
                 break;
             case KeyEvent.VK_D:
+                input('d');
                 break;
             case KeyEvent.VK_E:
+                input('e');
                 break;
             case KeyEvent.VK_F:
+                input('f');
                 break;
             case KeyEvent.VK_G:
+                input('g');
                 break;
             case KeyEvent.VK_H:
+                input('h');
                 break;
             case KeyEvent.VK_I:
+                input('i');
                 break;
             case KeyEvent.VK_J:
+                input('j');
                 break;
             case KeyEvent.VK_K:
+                input('k');
                 break;
             case KeyEvent.VK_L:
+                input('l');
                 break;
             case KeyEvent.VK_M:
+                input('m');
                 break;
             case KeyEvent.VK_N:
+                input('n');
                 break;
             case KeyEvent.VK_O:
+                input('o');
                 break;
             case KeyEvent.VK_P:
+                input('p');
                 break;
             case KeyEvent.VK_Q:
+                input('q');
                 break;
             case KeyEvent.VK_R:
+                input('r');
                 break;
             case KeyEvent.VK_S:
+                input('s');
                 break;
             case KeyEvent.VK_T:
+                input('t');
                 break;
             case KeyEvent.VK_U:
+                input('u');
                 break;
             case KeyEvent.VK_V:
+                input('v');
                 break;
             case KeyEvent.VK_W:
+                input('w');
                 break;
             case KeyEvent.VK_X:
+                input('x');
                 break;
             case KeyEvent.VK_Y:
+                input('y');
                 break;
             case KeyEvent.VK_Z:
+                input('z');
                 break;
             case KeyEvent.VK_BACK_SPACE:
+                backspace();
                 break;
             case KeyEvent.VK_ENTER:
+                System.arraycopy(wordLetters, 0, inputLetters, 0, 26);
+                if (isValidWord(grid[activeLine][0].getText() + grid[activeLine][1].getText() + grid[activeLine][2].getText() + grid[activeLine][3].getText() + grid[activeLine][4].getText())) {
+                    for (int i = 0; i < grid[0].length; i++) {
+                        if (grid[activeLine][i].getText().charAt(0) == word.charAt(i)) {
+                            grid[activeLine][i].setBackground(Color.green);
+                        } else if (inputLetters[grid[activeLine][i].getText().charAt(0) - 97] > 0) {
+                            grid[activeLine][i].setBackground(Color.yellow);
+                            inputLetters[grid[activeLine][i].getText().charAt(0) - 97] -= 1;
+                        }
+                        else {
+                            grid[activeLine][i].setBackground(Color.lightGray);
+                        }
+                    }
+                    activeLine += 1;
+                }
                 break;
         }
     }
@@ -173,4 +175,32 @@ public class Main extends JFrame implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent keyEvent) {}
+
+    public void input(char inputChar) {
+        if (grid[activeLine][0].getText().equals("")) {
+            grid[activeLine][0].setText(String.valueOf(inputChar));
+        } else if (grid[activeLine][1].getText().equals("")) {
+            grid[activeLine][1].setText(String.valueOf(inputChar));
+        } else if (grid[activeLine][2].getText().equals("")) {
+            grid[activeLine][2].setText(String.valueOf(inputChar));
+        } else if (grid[activeLine][3].getText().equals("")) {
+            grid[activeLine][3].setText(String.valueOf(inputChar));
+        } else if (grid[activeLine][4].getText().equals("")) {
+            grid[activeLine][4].setText(String.valueOf(inputChar));
+        }
+    }
+
+    public void backspace() {
+        if (!grid[activeLine][4].getText().equals("")) {
+            grid[activeLine][4].setText("");
+        } else if (!grid[activeLine][3].getText().equals("")) {
+            grid[activeLine][3].setText("");
+        } else if (!grid[activeLine][2].getText().equals("")) {
+            grid[activeLine][2].setText("");
+        } else if (!grid[activeLine][1].getText().equals("")) {
+            grid[activeLine][1].setText("");
+        } else if (!grid[activeLine][0].getText().equals("")) {
+            grid[activeLine][0].setText("");
+        }
+    }
 }
